f1c066298f9c09f6291fe52673e5e49447426e65
MykhailKo <tuareg1812@gmail.com>
2022-06-30 23:03:48 +0300

    chore: added bd table for addresses and bootstrap

--- bdd/DBBootstrap.config.js
+++ bdd/DBBootstrap.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-        /*
-          retriveData: (params) => ''
-        */
-      }
+  retriveTAaddressData: (params) => `update ta_addresses
+  set modified_date=now(), used=true
+  where id in (
+      select id from ta_addresses
+      where not used
+      and not cleanup_ready
+      and env = ${params.bddEnv}
+      ${params.tc_id ? `and tc_id = ${params.tc_id}` : ''}
+      ${params.city ? `and city = '${params.city}'` : ''}
+      ${params.province ? `and province = '${params.province}'` : ''}
+      ${params.type ? `and type = '${params.type}'` : ''}
+      limit 1
+  )
+  returning *`
+}
